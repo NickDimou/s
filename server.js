c9ab2b23439ca36ab3898ee146e9fa03434a87c4
Nick <NickDimou@users.noreply.github.com>
2026-03-19 08:11:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -15,30 +15,30 @@ const server = http.createServer((req, res) => {
 
   /* handle preflight requests */
   if (req.method === 'OPTIONS') {
-    res.writeHead(204);
-    return res.end();
+	res.writeHead(204);
+	return res.end();
   }
 
   /* fallback upload */
   if (req.method === 'POST') {
-    const id = Math.random().toString(36).slice(2);
-    const path = 'tmp_' + id;
+	const id = Math.random().toString(36).slice(2);
+	const path = 'tmp_' + id;
 
-    const stream = fs.createWriteStream(path);
-    req.pipe(stream);
+	const stream = fs.createWriteStream(path);
+	req.pipe(stream);
 
-    stream.on('finish', () => {
-      files[id] = path;
-      res.end(id);
-    });
+	stream.on('finish', () => {
+	  files[id] = path;
+	  res.end(id);
+	});
   }
 
   /* fallback download */
   else if (req.url.startsWith('/f/')) {
-    const id = req.url.split('/f/')[1];
-    if (!files[id]) return res.end();
+	const id = req.url.split('/f/')[1];
+	if (!files[id]) return res.end();
 
-    fs.createReadStream(files[id]).pipe(res);
+	fs.createReadStream(files[id]).pipe(res);
   }
 
   else res.end('stelno alive');
@@ -50,35 +50,44 @@ wss.on('connection', ws => {
   let room;
 
   ws.on('message', msg => {
-    const d = JSON.parse(msg);
-
-    if (d.join) {
-      room = d.join;
-      rooms[room] = rooms[room] || [];
-      rooms[room].push(ws);
-    }
-
-    if (d.signal && room) {
-      rooms[room].forEach(c => {
-        if (c !== ws && c.readyState === 1)
-          c.send(JSON.stringify({ signal: d.signal }));
-      });
-    }
-
-    /* fallback notify */
-    if (d.file && room) {
-      rooms[room].forEach(c => {
-        if (c !== ws && c.readyState === 1)
-          c.send(JSON.stringify({ file: d.file }));
-      });
-    }
+	  const d = JSON.parse(msg);
+  
+	  if (d.join) {
+		room = d.join;
+		rooms[room] = rooms[room] || [];
+		rooms[room].push(ws);
+	  }
+  
+	  if (d.signal && room) {
+		rooms[room].forEach(c => {
+		  if (c !== ws && c.readyState === 1)
+			c.send(JSON.stringify({ signal: d.signal }));
+		});
+	  }
+  
+	  /* fallback notify */
+	  if (d.file && room) {
+		rooms[room].forEach(c => {
+		  if (c !== ws && c.readyState === 1)
+			c.send(JSON.stringify({ file: d.file }));
+		});
+	  }
+  
+	  /* late peer discovery */
+	  if (d.checkPeers && room) {
+		rooms[room].forEach(c => {
+		  if (c !== ws && c.readyState === 1)
+			c.send(JSON.stringify({ checkPeers: true }));
+		});
+	  }
   });
 
+
   ws.on('close', () => {
-    if (room && rooms[room]) {
-      rooms[room] = rooms[room].filter(c => c !== ws);
-      if (!rooms[room].length) delete rooms[room];
-    }
+	if (room && rooms[room]) {
+	  rooms[room] = rooms[room].filter(c => c !== ws);
+	  if (!rooms[room].length) delete rooms[room];
+	}
   });
 });
 
